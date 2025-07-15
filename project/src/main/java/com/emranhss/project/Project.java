45b9ca2760e862d7438e8d48a89e9dcfa8948255
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-07-15 19:02:39 +0600

    addednew

---
+++ project/src/main/java/com/emranhss/project/Project.java
@@ -0,0 +1,16 @@
+package com.emranhss.project;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Project {
+
+
+    public static void main(String[] args) {
+       SpringApplication.run(SpringApplication.class);
+    }
+
+}
