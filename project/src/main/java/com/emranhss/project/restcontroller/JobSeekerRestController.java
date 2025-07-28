c3f2dbd683622a79c15291ae8744350d854e79e4
atiqurrahman109 <atiqurrahman.ar001@gmail.com>
2025-07-28 18:34:31 +0600

    adds

--- project/src/main/java/com/emranhss/project/restcontroller/JobSeekerRestController.java
+++ project/src/main/java/com/emranhss/project/restcontroller/JobSeekerRestController.java
@@ -1,5 +1,6 @@
 package com.emranhss.project.restcontroller;
 
+
 import com.emranhss.project.entity.JobSeeker;
 import com.emranhss.project.entity.User;
 import com.emranhss.project.service.UserService;
@@ -14,10 +15,8 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
 
 @RestController
 @RequestMapping("/api/jobseeker/")
@@ -32,12 +31,13 @@ public class JobSeekerRestController {
     public ResponseEntity<Map<String,String>> registerJobSeeker(
             @RequestPart(value = "user") String userJson,
             @RequestPart(value = "jobseeker") String jobSeekerJson,
-            @RequestPart(value = "photo")MultipartFile file
+            @RequestPart(value = "photo") MultipartFile file
 
     )
-        throws JsonProcessingException{
-        objectMapper objectMapper = new objectMapper();
-        User user=objectMapper.readValue(jobSeekerJson,jobSeeker.class);
+        throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user=objectMapper.readValue(userJson,User.class);
+        JobSeeker jobSeeker=objectMapper.readValue(jobSeekerJson,JobSeeker.class);
 
         try {
             userService.registerJobSeeker(user, file, jobSeeker);
--- project/src/main/java/com/emranhss/project/restcontroller/UserRestController.java
+++ project/src/main/java/com/emranhss/project/restcontroller/UserRestController.java
@@ -1,59 +1,59 @@
 package com.emranhss.project.restcontroller;
 
-<<<<<<< HEAD
 
 import com.emranhss.project.entity.User;
-=======
-import com.emranhss.project.entity.User;
 import com.emranhss.project.service.UserService;
->>>>>>> 8d70c07f5a7c06441d18095e23a545ae07b577e2
-import jakarta.persistence.EntityNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/user/")
-<<<<<<< HEAD
+@CrossOrigin("*")
 
 public class UserRestController {
-=======
-public class UserRestController {
 
->>>>>>> 8d70c07f5a7c06441d18095e23a545ae07b577e2
     @Autowired
     private UserService userService;
 
     @PostMapping
-<<<<<<< HEAD
-    public ResponseEntity<String> saveOrUpdate(@RequestBody User user){
-    try {
-        userService.saveOrUpdate(user);
-        return  ResponseEntity.ok("data saved");
-
-    } catch (EntityNotFoundException e){
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-    }
-
-    }
+    public ResponseEntity<Map<String, String>> saveUser(
+            @RequestPart(value = "user") String userJson,
+            @RequestParam(value = "photo")MultipartFile file
+            )
+            throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = objectMapper.readValue(userJson, User.class);
 
 
-
-
-
-=======
-    public ResponseEntity<String> saveOrUpdate(@RequestBody User user) {
         try {
-            userService.saveOrUpdate(user);
-            return ResponseEntity.ok("Data Saved");
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            userService.saveOrUpdate(user, file);
+            Map<String, String> response = new HashMap<>();
+            response.put("message", "User has been saved successfully");
+            return new ResponseEntity<>(response, HttpStatus.OK);
+        } catch (Exception e) {
+
+            Map<String, String> errorResponse = new HashMap<>();
+            errorResponse.put("message", e.getMessage());
+            return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+
         }
     }
->>>>>>> 8d70c07f5a7c06441d18095e23a545ae07b577e2
-}
+
+        @GetMapping("")
+        public ResponseEntity<List<User>> getAllUser () {
+            List<User> users = userService.findAll();
+            return ResponseEntity.ok(users);
+
+        }
+
+    }
+
